f94c618201a61c750db00d38bec485be865345d9
Nikola <nikola.tisic@gmail.com>
2015-03-25 22:52:45 +0100

    Uradio klasu Program i kosture za osobe TREBA IH SVE IZVESTI IZ KLASE OSOBA STO SADA NIJE URADJENO!

--- toalet/Male.java
+++ toalet/Male.java
@@ -1,6 +1,41 @@
 package toalet;
 
-public class Male{
+public class Male extends Thread{
 	
+	private static int lastID = 0;
+	private int ID = lastID++;
+	private int maxTime = 5;
+	private String state;
+	
+	private void printMe(){
+		System.out.println(this);
+	}
+	
+	public Male(){
+		start();
+	}
+	
+	public void run(){
+		
+		try{
+			
+			state = "pocinje";
+			printMe();
+			
+			//entry protocol
+			
+			sleep((long)(Math.random()*maxTime));
+			
+			//exit protocol
+			state = "zavrsio";
+			printMe();
+			
+		}catch (InterruptedException e){}
+		
+	}
+	
+	public String toString(){
+		return ID + "M: " + state;
+	}
 	
 }
--- toalet/Program.java
+++ toalet/Program.java
@@ -1,6 +1,75 @@
 package toalet;
 
+import java.util.concurrent.Semaphore;
+
 public class Program{
 	
+	//////////////////////////////////////////
+	//				SHARED DATA				//
+	//////////////////////////////////////////
+	
+	public static Semaphore mutex = new Semaphore(1);		//medjusobno iskljucenje za deljene podatke
+	public static Semaphore groupA = new Semaphore(1);		//uslovna sinhronizacija: Neodgovarajuca grupa za sve osim za decu
+	public static Semaphore groupC = new Semaphore(1);		//uslovna sinhronizacija: Neodgovarajuca grupa za decu
+	public static Semaphore firstM = new Semaphore(1);
+	public static Semaphore firstF = new Semaphore(1);
+	public static Semaphore firstC = new Semaphore(1);
+	public static Semaphore secondM = new Semaphore(0);
+	public static Semaphore secondF = new Semaphore(0);
+	public static Semaphore secondC = new Semaphore(0);
+	
+	
+	//broj Male, Frmale, Child, Repairman osoba koje su u kupatilu, respektivno
+	public static int cntM = 0;
+	public static int cntF = 0;
+	public static int cntC = 0;
+	public static int cntR = 0;
+	
+	//broj Male, Frmale, Child, Repairman osoba koje cekaju, respektivno
+	public static int waitM = 0;
+	public static int waitF = 0;
+	public static int waitC = 0;
+	public static int waitR = 0;
+	
+	//ukoliko dete i roditelj cekaju da udju ciji je red
+	public static boolean childTurn = true;
+	
+	public static void main(String[] args){
+		
+		//broj blokova
+		int rnds = 3;
+		
+		//broj instanciranih osoba u bloku
+		int round [] = {5, 10, 15};
+		
+		//pauza izmedju blokova
+		long timeout[] = {15,20};
+		
+		for (int rnd = 1; rnd<rnds; rnd++){
+			System.out.println("============= BLOCK "+rnd+" START  ============");
+			//kreiraj i startuj osobe
+			for (int i=0; i<round[rnd]; i++){
+				switch ((int)(Math.random()*4)){
+				case 0:
+					new Male();break;
+				case 1:
+					new Female();break;
+				case 2:
+					new Child();break;
+				case 3:
+					new Repairman();
+				}
+			}
+			
+			//timeout
+			try {
+				//ako nije poslednji blok
+				if (rnd<rnds-1)
+					Thread.sleep(timeout[rnd]*1000);
+			} catch (InterruptedException e) {}
+			System.out.println("============= BLOCK "+rnd+" FINISH ============");
+		}
+		
+	}
 	
 }
